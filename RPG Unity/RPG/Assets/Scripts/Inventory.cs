a78712c1f2d605a6b5ff137870b4c4137b358f37
Sethanic <cian.perill@gmail.com>
2016-04-09 09:44:20 +0100

    Updated the game

---
+++ RPG Unity/RPG/Assets/Scripts/Inventory.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+    public int slotsX, slotsY;
+    public GUISkin skin;
+
+    public List<Items> slots = new List<Items>();
+    public List<Items> inventory = new List<Items>();
+    private bool showInventory;
+    private ItemDatabase database;
+
+	void Start () 
+    {
+        for (int i = 0; i < (slotsX * slotsY); i++)
+        {
+            slots.Add(new Items());
+        }
+        database = GameObject.FindGameObjectWithTag("Item Database").GetComponent<ItemDatabase>();
+        inventory.Add(database.items[0]);
+        inventory.Add(database.items[1]);
+	}
+
+    void Update ()
+    {
+        if (Input.GetButtonDown("Inventory"))
+        {
+            showInventory = !showInventory;
+        }
+    }
+	
+	void OnGUI () 
+    {
+        if (showInventory)
+        {
+            DrawInventory();
+        }
+	}
+
+    void DrawInventory ()
+    {
+        for (int x  = 0; x < slotsX; x++)
+        {
+            for (int y = 0; x < slotsY; y++)
+            {
+                GUI.Box(new Rect(x *  20, y * 20, 20, 20), y.ToString());
+            }
+        }
+
+    }
+}
---
+++ RPG Unity/RPG/Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour {
+    public List<Items> items = new List<Items>();
+
+    void Awake()
+    {
+                        //Name    id  description                                     damage    sharpness     armour   type
+        items.Add(new Items("Shirt", 0, "A common shirt, worn by prisoners and peasants.", 0, 0, 2, Items.ItemType.Armour));
+        items.Add(new Items("Glove", 1, "A glove of leather. Makes you sweat easily.", 0, 0, 1, Items.ItemType.Armour));
+    }
+}
